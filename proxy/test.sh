81296db61ff7e2a2f7b9bd63e36fb15d0840c6d3
Jesse Shawl <jesse@jesse.sh>
2025-12-06 13:53:33 -0500

    add test

---
+++ proxy/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+IMAGE=$(sed -n 's/^FROM \([^ ]*\).*/\1/p' "$SCRIPT_DIR/Dockerfile")
+
+test_conf() {
+    echo "🧪 testing $SCRIPT_DIR/$1"
+    docker run --rm \
+        --add-host="server:127.0.0.1" \
+        --add-host="frontend:127.0.0.1" \
+        -v $SCRIPT_DIR/$1:/etc/nginx/nginx.conf \
+        --entrypoint bash nginx:1.29.3 \
+        -c "nginx -t"
+    echo -e "✅ $1 is ok\n"
+}
+
+cleanup() {
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "❌ test failed with exit code $exit_code"
+    fi
+}
+
+trap cleanup EXIT
+
+test_conf nginx.dev.conf
+test_conf nginx.conf
