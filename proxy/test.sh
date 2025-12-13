a18739ea53746ea9a70c9e973b299f0413319025
Jesse Shawl <jesse@jesse.sh>
2025-12-13 05:48:41 -0500

    fix test

--- proxy/test.sh
+++ proxy/test.sh
@@ -12,6 +12,7 @@ test_conf() {
     docker run --rm \
         -v $SCRIPT_DIR/Caddyfile:/etc/caddy/Caddyfile \
         --entrypoint sh proxy \
+        -e AUTO_HTTPS=off \
         -c "caddy validate --config /etc/caddy/Caddyfile"
     echo -e "✅ $1 is ok\n"
 }
